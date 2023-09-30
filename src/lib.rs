276431b70bc48d639fb877ac1d43830041015242
Milad Nekofar <milad@nekofar.com>
2023-09-30 11:34:25 +0330

    refactor: implement basic routing functionality
    This commit introduces a basic routing infrastructure to the application in lib.rs. The main function now consists of a router that accepts GET requests at the root ("/") and responds with "Hello, World!". This step was necessary to establish a more flexible structure for adding different endpoints in the future.

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,10 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+    let router = Router::new();
+
+    router
+        .get("/", |_, _| Response::ok("Hello, World!"))
+        .run(req, env)
+        .await
 }
