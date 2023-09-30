df87bde8d23ba9daa037440603e8a5a070e4234b
Milad Nekofar <milad@nekofar.com>
2023-09-30 17:38:56 +0330

    style: format code for better readability
    Adjusted the indentation and spacing within the function 'main'. This reformatting is intended to enhance code readability and maintainability. It should make it easier for developers to understand the routing function structure and the async block within it.

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,17 @@ use worker::{console_log, event, Context, Env, Request, Response, Result, Router
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
-    let router = Router::new();
+  let router = Router::new();
 
-    router
-        .get("/", |_, _| Response::ok("Hello, World!"))
-        .get_async("/:sqid", |_, ctx| async move {
-            if let Some(sqid) = ctx.param("sqid") {
-                let sqids = Sqids::default();
-                let numbers = sqids.decode(&sqid);
+  router
+    .get("/", |_, _| Response::ok("Hello, World!"))
+    .get_async("/:sqid", |_, ctx| async move {
+      if let Some(sqid) = ctx.param("sqid") {
+        let sqids = Sqids::default();
+        let numbers = sqids.decode(&sqid);
 
-                console_log!("{:?}", numbers)
-            }
+        console_log!("{:?}", numbers)
+      }
 
             Response::error("Bad Request", 400)
         })
