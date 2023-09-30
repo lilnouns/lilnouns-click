fffe8fed17a7ac99d7f590911dfa407efcd435eb
Milad Nekofar <milad@nekofar.com>
2023-09-30 16:33:31 +0330

    refactor: add SQID parameter decoding to router
    SQIDS dependency was added to facilitate sqid parameter decoding. Additional router handler was implemented to decode sqid parameters from incoming requests using newly introduced SQIDS dependency. This allows the app to handle yet another type of requests, adding to its functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use worker::*;
+use sqids::Sqids;
+use worker::{console_log, event, Context, Env, Request, Response, Result, Router};
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
@@ -6,6 +7,16 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 
     router
         .get("/", |_, _| Response::ok("Hello, World!"))
+        .get_async("/:sqid", |_, ctx| async move {
+            if let Some(sqid) = ctx.param("sqid") {
+                let sqids = Sqids::default();
+                let numbers = sqids.decode(&sqid);
+
+                console_log!("{:?}", numbers)
+            }
+
+            Response::error("Bad Request", 400)
+        })
         .run(req, env)
         .await
 }
