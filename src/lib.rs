faaa81c4e7d327d9e803f58270372a32bd6a8c13
Milad Nekofar <milad@nekofar.com>
2023-10-02 20:04:01 +0330

    refactor: add redirect from root to a Lil Nouns URL
    A dependency on the `url` library was introduced, and the response to the root ("/") route was changed. Instead of responding with "Hello, World!", the server now redirects to "https://lilnouns.wtf". This is done to guide the users directly to the relevant content."

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use url::Url;
 use worker::{event, Context, Env, Request, Response, Result, Router};
 
 mod queries;
@@ -9,7 +10,9 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
   let router = Router::new();
 
   router
-    .get("/", |_, _| Response::ok("Hello, World!"))
+    .get("/", |_, _| {
+      Response::redirect(Url::parse("https://lilnouns.wtf")?)
+    })
     .get_async("/:sqid", routes::handle_redirect)
     .post_async("/", routes::handle_creation)
     .run(req, env)
