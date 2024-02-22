a18bf9e0f479ebb3509931a94ff8db24b47626bb
Milad Nekofar <milad@nekofar.com>
2024-02-22 11:42:38 +0330

    refactor: update code to solve spacing issues and indentations

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,17 @@ mod utils;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
-    let router = Router::new();
+  let router = Router::new();
 
-    router
-        .get("/", |_, _| {
-            Response::redirect(Url::parse(
-                "https://lilnouns.wtf?utm_source=farcaster&utm_medium=social",
-            )?)
-        })
-        .get_async("/:sqid", routes::handle_redirect)
-        .get_async("/:sqid/og.png", routes::handle_og_image)
-        .post_async("/", routes::handle_creation)
-        .run(req, env)
-        .await
+  router
+    .get("/", |_, _| {
+      Response::redirect(Url::parse(
+        "https://lilnouns.wtf?utm_source=farcaster&utm_medium=social",
+      )?)
+    })
+    .get_async("/:sqid", routes::handle_redirect)
+    .get_async("/:sqid/og.png", routes::handle_og_image)
+    .post_async("/", routes::handle_creation)
+    .run(req, env)
+    .await
 }
